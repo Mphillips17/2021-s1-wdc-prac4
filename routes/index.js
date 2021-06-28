68729fe1122cb306687975aba8d4b189770dbad0
Mphillips17 <a1706543@student.email.edu.au>
2021-06-28 14:53:41 +0000

    save

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,7 @@ router.get('/color.html', function(req,res){
 
     counter++;
     res.send('<!DOCTYPE html> \
-        <html> \
+        <html lang="en"> \
           <head> \
             <title>Title</title> \
             <style></style> \
@@ -46,4 +46,51 @@ router.get('/color.html', function(req,res){
 
 });
 
+var li = '';
+router.get('/log.html', function(req,res){
+   var date = Date();
+   li = li + '<li>' + Date() + '</li>';
+
+
+    res.send('<!DOCTYPE html> \
+        <html lang="en"> \
+          <head> \
+            <title>Title</title> \
+            <style></style> \
+          </head> \
+          <body> \
+          <ul>\
+          '+li+'\
+          </ul>\
+          <body>\
+        </html>');
+});
+
+//task3.4
+var visited = false;
+router.get('/first.html',function(req, res){
+
+    if(visited == false){
+        res.send('<!DOCTYPE html> \
+        <html  lang="en"> \
+          <head> \
+            <title>Title</title> \
+            <link rel="stylesheet" href="/stylesheets/style.css"> \
+          </head> \
+          <body> \
+          <h1>Welcome</h1>\
+          <a href="/main.html">Main</a>\
+          <body>\
+        </html>');
+        visited=true;
+    }
+    else{
+
+        res.redirect('/main.html');
+    }
+});
+
+router.get('/main.html',function(req,res){
+    res.render('main', { title: 'Title' });
+});
 module.exports = router;
