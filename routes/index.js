22dbbdb7faad62fdb7de23febcfa3a7952b97fd1
Mphillips17 <a1706543@student.email.edu.au>
2021-06-28 13:16:42 +0000

    save

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+let counter = 0;
+router.get('/hitcount',function(req,res,next){
+    counter ++;
+    res.send(string(counter));
+});
+
+router.get('last.txt', function(req,res){
+   res.send(string);
+   string = Date();
+});
+
 module.exports = router;
